f0541adf3a0897b8baff64541d80cefe07081ab8
Janez Troha <dz0ny@ubuntu.si>
2021-07-12 21:50:40 +0200

    Initial Commit

---
+++ Pareto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pareto
+//
+//  Created by Janez Troha on 12/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pareto/ParetoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParetoApp.swift
+//  Pareto
+//
+//  Created by Janez Troha on 12/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct ParetoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
