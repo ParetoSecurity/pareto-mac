9a0f7a715bbc3d296e30d74d7e766fc8f877d19a
Janez Troha <dz0ny@ubuntu.si>
2021-12-13 13:37:13 +0100

    Handle non semvar version of Firefox
    Closes https://github.com/ParetoSecurity/pareto-mac/issues/87

--- Pareto/Checks/Firefox.swift
+++ Pareto/Checks/Firefox.swift
@@ -25,6 +25,17 @@ class AppFirefoxCheck: AppCheck {
         "768a574c-75a2-536d-8785-ef9512981184"
     }
 
+    override var currentVersion: Version {
+        if applicationPath == nil {
+            return Version(0, 0, 0)
+        }
+        let v = appVersion(path: applicationPath!)!.split(separator: ".")
+        if v.count == 2 {
+            return Version(Int(v[0]) ?? 0, Int(v[1]) ?? 0, 0)
+        }
+        return Version(Int(v[0]) ?? 0, Int(v[1]) ?? 0, Int(v[2]) ?? 0)
+    }
+
     override func getLatestVersion(completion: @escaping (String) -> Void) {
         let url = "https://product-details.mozilla.org/1.0/firefox_history_stability_releases.json"
         os_log("Requesting %{public}s", url)
