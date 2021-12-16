26f5444249d21e088cbd20c4230c022fba785504
Janez Troha <dz0ny@ubuntu.si>
2021-12-16 14:57:40 +0100

    Use global update check for Slack
    Supports both .dmg install and App store install.
    
    Closes https://github.com/ParetoSecurity/pareto-mac/issues/80

--- Pareto/Checks/Software Updates/Slack.swift
+++ Pareto/Checks/Software Updates/Slack.swift
@@ -5,6 +5,10 @@
 //  Created by Janez Troha on 11/11/2021.
 //
 
+import Alamofire
+import os.log
+import Regex
+
 class AppSlackCheck: AppCheck {
     static let sharedInstance = AppSlackCheck()
 
@@ -14,5 +18,23 @@ class AppSlackCheck: AppCheck {
 
     override var UUID: String {
         "9894a05b-964d-5c19-bef7-53112207d271"
+    }
+
+    override func getLatestVersion(completion: @escaping (String) -> Void) {
+        let url = "https://slack.com/release-notes/mac"
+        let versionRegex = Regex("<h2>Slack ?([\\.\\d]+)</h2>")
+        os_log("Requesting %{public}s", url)
+        AF.request(url).responseString(queue: AppCheck.queue, completionHandler: { response in
+            if response.error == nil {
+                let html = response.value ?? "<h2>Slack 1.23.0</h2>"
+                let version = versionRegex.firstMatch(in: html)?.groups.first?.value ?? "1.23.0"
+                os_log("%{public}s version=%{public}s", self.appBundle, version)
+                completion(version)
+            } else {
+                os_log("%{public}s failed: %{public}s", self.appBundle, response.error.debugDescription)
+                completion("0.0.0")
+            }
+
+        })
     }
 }
