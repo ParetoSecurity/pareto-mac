82e8f4b5e4ada46ca2910d91011c0a8132aa9929
Janez Troha <dz0ny@ubuntu.si>
2022-01-19 15:02:59 +0100

    Fix tests

--- Pareto/Checks/AppCheck.swift
+++ Pareto/Checks/AppCheck.swift
@@ -10,7 +10,6 @@ import AppKit
 import Cocoa
 import Combine
 import Defaults
-import ExceptionCatcher
 import Foundation
 import os.log
 import OSLog
@@ -107,15 +106,9 @@ class AppCheck: ParetoCheck, AppCheckProtocol {
         if isInstalled {
             let app = applicationPath!.components(separatedBy: "/Contents/")[0]
             let weekAgo = Date().addingTimeInterval(-7 * 24 * 60 * 60)
-            do {
-                let lastUse = try ExceptionCatcher.catch { () -> Date in
-                    let attributes = NSMetadataItem(url: URL(fileURLWithPath: app))
-                    return attributes?.value(forKey: "kMDItemLastUsedDate") as! Date
-                }
-                return lastUse >= weekAgo
-            } catch {
-                return false
-            }
+            let attributes = NSMetadataItem(url: URL(fileURLWithPath: app))
+            let lastUse = attributes?.value(forKey: "kMDItemLastUsedDate") as? Date
+            return lastUse ?? Date() >= weekAgo
         }
         return false
     }
