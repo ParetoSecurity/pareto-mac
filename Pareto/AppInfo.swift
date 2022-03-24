3e7e7145822a29e30b3c1cf2203fb700c8f5e287
Janez Troha <dz0ny@ubuntu.si>
2022-03-24 10:31:19 +0100

    Use dynamic machine name
    Closes https://github.com/teamniteo/pareto/issues/298

--- Pareto/AppInfo.swift
+++ Pareto/AppInfo.swift
@@ -17,7 +17,7 @@ import Version
 enum AppInfo {
     static let appVersion: String = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as! String
     static let buildVersion: String = Bundle.main.infoDictionary?["CFBundleVersion"] as! String
-    static let machineName: String = Host.current().localizedName!
+    static var machineName: String { Host.current().localizedName! }
     static let macOSVersion = ProcessInfo.processInfo.operatingSystemVersion
     static let macOSVersionString = "\(macOSVersion.majorVersion).\(macOSVersion.minorVersion).\(macOSVersion.patchVersion)"
     static var isRunningTests: Bool {
