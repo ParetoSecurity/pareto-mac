56fb8a349412a5b80b9e52de47be1541a8049e03
Janez Troha <dz0ny@ubuntu.si>
2022-05-18 12:00:13 +0200

    Add App store updates check

--- Pareto/Checks/System Integrity/SecureiTerm.swift
+++ Pareto/Checks/System Integrity/SecureiTerm.swift
@@ -15,15 +15,19 @@ class SecureiTermCheck: ParetoCheck {
     override var UUID: String {
         "6cbe1cfd-ff28-4cc7-8998-5d72e608b28d"
     }
-    
+
     override var TitleON: String {
-        "Other apps cannot read from iTerm.app"
+        "iTerm uses secure entry"
     }
-    
+
     override var TitleOFF: String {
-        "Other apps can read from iTerm.app"
+        "iTerm is not using secure entry"
     }
-    
+
+    override var isRunnable: Bool {
+        AppiTermCheck.sharedInstance.isInstalled
+    }
+
     override func checkPasses() -> Bool {
         if let enabled = readDefaultsNative(path: "com.googlecode.iterm2", key: "\"Secure Input\"") {
             os_log("Secure Input, status %{enabled}s", log: Log.check, enabled)
