7b6a8f3d1246e5a3a306da56d49ef98f72c24ed9
Janez T <hey@dz0ny.dev>
2025-12-19 15:39:33 +0100

    chore: update keychain cleanup commands in workflows and enhance snooze time tests for accuracy

--- ParetoSecurityTests/ParetoSecurityTests.swift
+++ ParetoSecurityTests/ParetoSecurityTests.swift
@@ -33,16 +33,46 @@ class ParetoSecurityTests: XCTestCase {
         // Reset snooze before testing
         Defaults[.snoozeTime] = 0
 
-        handlers.snoozeOneDay()
-        XCTAssertGreaterThan(Defaults[.snoozeTime], 0)
+        // Tolerance in milliseconds (2 seconds to account for test execution time)
+        let tolerance = 2000
 
+        // Expected durations in milliseconds
+        let oneHourMs = 3600 * 1000
+        let oneDayMs = 3600 * 24 * 1000
+        let oneWeekMs = 3600 * 24 * 7 * 1000
+
+        // Test snoozeOneHour
+        var beforeTime = Date().currentTimeMs()
         handlers.snoozeOneHour()
-        XCTAssertGreaterThan(Defaults[.snoozeTime], 0)
+        var expectedMin = beforeTime + oneHourMs - tolerance
+        var expectedMax = beforeTime + oneHourMs + tolerance
+        XCTAssertGreaterThanOrEqual(Defaults[.snoozeTime], expectedMin,
+            "snoozeOneHour should set time to approximately 1 hour in the future")
+        XCTAssertLessThanOrEqual(Defaults[.snoozeTime], expectedMax,
+            "snoozeOneHour should set time to approximately 1 hour in the future")
 
+        // Test snoozeOneDay
+        beforeTime = Date().currentTimeMs()
+        handlers.snoozeOneDay()
+        expectedMin = beforeTime + oneDayMs - tolerance
+        expectedMax = beforeTime + oneDayMs + tolerance
+        XCTAssertGreaterThanOrEqual(Defaults[.snoozeTime], expectedMin,
+            "snoozeOneDay should set time to approximately 24 hours in the future")
+        XCTAssertLessThanOrEqual(Defaults[.snoozeTime], expectedMax,
+            "snoozeOneDay should set time to approximately 24 hours in the future")
+
+        // Test snoozeOneWeek
+        beforeTime = Date().currentTimeMs()
         handlers.snoozeOneWeek()
-        XCTAssertGreaterThan(Defaults[.snoozeTime], 0)
+        expectedMin = beforeTime + oneWeekMs - tolerance
+        expectedMax = beforeTime + oneWeekMs + tolerance
+        XCTAssertGreaterThanOrEqual(Defaults[.snoozeTime], expectedMin,
+            "snoozeOneWeek should set time to approximately 7 days in the future")
+        XCTAssertLessThanOrEqual(Defaults[.snoozeTime], expectedMax,
+            "snoozeOneWeek should set time to approximately 7 days in the future")
 
+        // Test unsnooze
         handlers.unsnooze()
-        XCTAssertEqual(Defaults[.snoozeTime], 0)
+        XCTAssertEqual(Defaults[.snoozeTime], 0, "unsnooze should reset snooze time to 0")
     }
 }
